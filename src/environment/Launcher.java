f8221e634ba30c903e8557800de1ba62acdabf97
Kristian <krisisurf@gmail.com>
2022-08-13 20:24:17 +0300

    Rename title of JFrame

--- src/environment/Launcher.java
+++ src/environment/Launcher.java
@@ -3,7 +3,7 @@ package environment;
 public class Launcher {
 
 	public static void main(String[] args) {
-		ViewController g = new ViewController("Skyblock Warrior 2D", 1000, 800);
+		ViewController g = new ViewController("Ray-Casting-2D DEMO", 1000, 800);
 		g.start();
 	}
 
